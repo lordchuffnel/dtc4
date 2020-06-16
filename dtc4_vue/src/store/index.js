55108fa69cd0b6e26ddfa100e86066dc38dfb4ea
frank <paintersmechanic@gmail.com>
2020-06-16 13:48:22 -0700

    removed vue-cli-plugin-axios

--- dtc4_vue/src/store/index.js
+++ dtc4_vue/src/store/index.js
@@ -1,14 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    timecards: [],
+    user: null,
+    loading: false
   },
   mutations: {
+    AddTimecard: (state, timecard) => {
+      state.timecards.push(timecard)
+    },
+    RemoveTimecard: (state, timecard) => {
+      state.timecards.remove(timecard)
+    },
+    SetUser: (state, user) => {
+      state.user = user
+    },
+    SetLoading: (state, bool) => {
+      state.loading = bool
+    }
   },
   actions: {
+    getTimecardInfo({ state, commit }) {
+      axios
+        .get('127.0.0.1:8000/api/timecards/')
+        .then(res => res.data)
+        .then(el => {
+          timecard.id === el.id
+          commit()
+      })
+    }
   },
   modules: {
   }
