d03e01850c54b53faad1f210f798b799ed29dd53
frank <paintersmechanic@gmail.com>
2020-06-19 13:21:00 -0700

    trying fetch

--- dtc4_vue/src/components/Timecards.vue
+++ dtc4_vue/src/components/Timecards.vue
@@ -1,26 +1,38 @@
 <template>
   <div>
     <h1>Timecards</h1>
-    <div v-for="timecard in timecards" :key="timecard.id">
-      <ul>
-        <li>{{ t.date }}</li>
-        <li>{{ t.start_time }}</li>
-      </ul>
+    <div v-for="timecard in timecards" :key="timecard.id" :timecard="timecard">
+      <!-- <ul>
+        <li>{{ timecard.date }}</li>
+        <li>{{ ttimecard.start_time }}</li>
+      </ul> -->
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import {  mapState } from 'vuex';
 export default {
   name: 'Timecards',
   data() {
     return {
       timecards: [],
+      tc: null
     };
   },
   methods: {
-    ...mapActions(['getTimecardInfo']),
+    getMovies() {
+      fetch('http://127.0.0.1:8000/api/timecards/', {
+        method: 'GET',
+        headers: {
+          username: 'frank',
+          password: 'alleyrat'
+        },
+      }).then(res => res.json())
+      .then(res => {
+        this.tc = res;
+      })
+    }
   },
   computed: { ...mapState(['timecards', 'user', 'loading']) },
 };
